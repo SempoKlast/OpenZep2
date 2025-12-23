a1162d62a12fa30212463ee916f4fb1eb49c0203
SempoKlast <sempoklast@gmail.com>
2025-12-23 08:51:48 +0100

    just

--- contracts/erc721.sol
+++ contracts/erc721.sol
@@ -1,31 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.2/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.2/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.2/contracts/access/Ownable.sol";
 
-contract SimpleNFT is ERC721, Ownable {
-    uint256 public nextTokenId = 1;
-    string private baseTokenURI;
-
+contract SimpleMintToken is ERC20, Ownable {
     constructor(
         string memory name_,
         string memory symbol_,
-        string memory baseURI_
-    ) ERC721(name_, symbol_) Ownable(msg.sender) {
-        baseTokenURI = baseURI_;
+        uint256 initialSupply
+    ) ERC20(name_, symbol_) Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
     }
 
-    function setBaseURI(string calldata baseURI_) external onlyOwner {
-        baseTokenURI = baseURI_;
-    }
-
-    function mint(address to) external onlyOwner returns (uint256 tokenId) {
-        tokenId = nextTokenId++;
-        _safeMint(to, tokenId);
-    }
-
-    function _baseURI() internal view override returns (string memory) {
-        return baseTokenURI;
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
     }
 }
